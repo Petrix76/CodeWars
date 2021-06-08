82d1be77c75e363d08d8cdcbd65fc3d6955999f1
Petr Jeníček <Jenda636cz@gmail.com>
2021-06-08 09:41:36 +0200

    init multiples

---
+++ src/kyu6/Multiples3Or5.java
@@ -0,0 +1,9 @@
+package kyu6;
+
+public class Multiples3Or5 {
+
+    public int solution(int number) {
+        return 0;
+    }
+
+}
---
+++ test/kyu6/Multiples3Or5Test.java
@@ -0,0 +1,17 @@
+package kyu6;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Petr
+ */
+public class Multiples3Or5Test {
+    
+    @Test
+    public void test() {
+      assertEquals(23, new Multiples3Or5().solution(10));
+    }
+
+}
