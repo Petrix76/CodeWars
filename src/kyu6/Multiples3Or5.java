9a113ecb402d5fd8a381e03c48865f3fd1334e49
Petr Jeníček <Jenda636cz@gmail.com>
2021-06-08 11:04:30 +0200

    Multiples 3 or 5 done.

--- src/kyu6/Multiples3Or5.java
+++ src/kyu6/Multiples3Or5.java
@@ -6,21 +6,31 @@ import java.util.stream.IntStream;
 
 public class Multiples3Or5 {
     
-    private final Set<Integer> set = new HashSet();
-    
     public int solution(int number) {    
 
 //        return IntStream.range(0, number)
 //                .filter((num) -> (num % 5 == 0) || (num % 3 == 0))
 //                .sum();
         
-        for (int i = 1; i < number; i++) {
-            if(i % 5 == 0) set.add(i);
-            if(i % 3 == 0) set.add(i);
+        int sum = 0;
+        
+        for (int i = 0; i < number; i++) {
+            if(i % 3 == 0 || i % 5 == 0){
+                sum += i;
+            }
         }
+        
+        return sum;
 
-        return set.stream()
-                 .reduce(0, (acc, el) -> acc + el);
+//        Set<Integer> set = new HashSet();
+//
+//        for (int i = 1; i < number; i++) {
+//            if(i % 5 == 0) set.add(i);
+//            if(i % 3 == 0) set.add(i);
+//        }
+//
+//        return set.stream()
+//                 .reduce(0, (acc, el) -> acc + el);
     }
 
 }
