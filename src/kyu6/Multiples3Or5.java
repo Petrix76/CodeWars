f76c653c151c700976c81add3031058f709c6b5f
Petr Jeníček <Jenda636cz@gmail.com>
2021-06-08 10:50:55 +0200

    Multiples 3 or 5 done.

--- src/kyu6/Multiples3Or5.java
+++ src/kyu6/Multiples3Or5.java
@@ -1,9 +1,21 @@
 package kyu6;
 
-public class Multiples3Or5 {
+import java.util.HashSet;
+import java.util.Set;
 
-    public int solution(int number) {
-        return 0;
+public class Multiples3Or5 {
+    
+    private final Set<Integer> set = new HashSet();
+    
+    public int solution(int number) {    
+        
+        for (int i = 1; i < number; i++) {
+            if(i % 5 == 0) set.add(i);
+            if(i % 3 == 0) set.add(i);
+        }
+
+        return set.stream()
+                 .reduce(0, (acc, el) -> acc + el);
     }
 
 }
